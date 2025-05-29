c114922d1432be5b60219ffd64cdfb6019d1b904
Yu Li <BFlameSwift@163.com>
2025-05-29 20:24:54 +0800

    Update TSP_IO.h

--- Search/code/include/TSP_IO.h
+++ Search/code/include/TSP_IO.h
@@ -47,8 +47,8 @@ int Inst_Num_Per_Batch=std::ceil(Total_Instance_Num / 32);
 int Index_In_Batch;
 int rec_only;
 int restart_reconly;
-int use_greedy_rollout = 0; // whether to evaluate leaf nodes by greedy rollout
-int use_2opt_rollout = 0; // whether to apply 2-Opt after greedy rollout
+int use_greedy_rollout = 1; // whether to evaluate leaf nodes by greedy rollout
+int use_2opt_rollout = 1; // whether to apply 2-Opt after greedy rollout
 
 /*
  * Here we define new array to store the recomend citys and corresponding value
