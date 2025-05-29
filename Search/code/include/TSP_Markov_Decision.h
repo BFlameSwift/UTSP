661bfa4ce7368b2f3b2b1e2d2e1c69fa9c6d0563
Yu Li <BFlameSwift@163.com>
2025-05-29 21:57:04 +0800

    Revert "Fix TSP solving by simplifying search"

--- Search/code/include/TSP_Markov_Decision.h
+++ Search/code/include/TSP_Markov_Decision.h
@@ -7,25 +7,43 @@ void Jump_To_Random_State()
 
 Distance_Type Markov_Decision_Process(int Inst_Index)
 {
-    MCTS_Init(Inst_Index);
-    Generate_Initial_Solution();
+	MCTS_Init(Inst_Index);                      // Initialize MCTS parameters
+	Generate_Initial_Solution();      // State initialization of MDP
+	Local_Search_by_2Opt_Move();	  // 2-opt based local search within small neighborhood	
+	MCTS();		                      // Tageted sampling via MCTS within enlarged neighborhood
 
-    Distance_Type best = Greedy_Rollout_2Opt(Start_City);
-    Store_Best_Solution();
-
-    for(int i=0;i<3;++i)
-    {
-        Jump_To_Random_State();
-        Distance_Type dist = Greedy_Rollout_2Opt(Start_City);
-        if(dist < best)
-        {
-            best = dist;
-            Store_Best_Solution();
+	// Repeat the following process until termination
+    int jump = 0;
+	while(((double)clock()-Current_Instance_Begin_Time) /CLOCKS_PER_SEC<Param_T*Virtual_City_Num)
+	{
+        // Here we restart the max depth, and rec_only
+        if (restart) {
+            int old_rec_only = rec_only;
+            if (restart_reconly) {
+                rec_only = rand() % 2;
+                if (rec_only != old_rec_only) {
+                    Identify_Candidate_Set();
+                }
+            }
+            // Random 
+            //Max_Depth = 100 + (rand() % 60);
+            // Random v2
+            Max_Depth = 10 + (rand() % 80);
         }
-    }
-
-    Restore_Best_Solution();
-    return best;
+        ++jump;
+		Jump_To_Random_State();		
+		Local_Search_by_2Opt_Move();		
+		MCTS();
+	}
+    cout << "jump:" << jump <<endl;
+	
+	// Copy information of the best found solution (stored in Struct_Node *Best_All_Node ) to Struct_Node *All_Node 
+	Restore_Best_Solution();
+	
+	if(Check_Solution_Feasible())
+		return Get_Solution_Total_Distance();
+	else
+		return Inf_Cost;
 }
 
 
